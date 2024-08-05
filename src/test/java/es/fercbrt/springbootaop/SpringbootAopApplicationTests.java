610bc0cb682cca67cc3f19df2b166121e737739f
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-05 17:00:32 +0200

    Initial commit

---
+++ src/test/java/es/fercbrt/springbootaop/SpringbootAopApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fercbrt.springbootaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
