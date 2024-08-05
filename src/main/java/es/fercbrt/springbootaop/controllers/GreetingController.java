8cf733378ef94a42eb25db257901e8717fb2ccb9
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-05 17:07:08 +0200

    Service injected

--- src/main/java/es/fercbrt/springbootaop/controllers/GreetingController.java
+++ src/main/java/es/fercbrt/springbootaop/controllers/GreetingController.java
@@ -1,14 +1,20 @@
 package es.fercbrt.springbootaop.controllers;
 
+import es.fercbrt.springbootaop.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
+    @Autowired
+    private GreetingService greetingService;
+
     @GetMapping("/greeting")
-    public ResponseEntity<?> greeting() {
-        return ResponseEntity.ok("Hello, World!");
+    public ResponseEntity<?> greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return ResponseEntity.ok(greetingService.sayHello(name));
     }
 }
